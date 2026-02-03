0ff11da663798c6b41684602962aef36fee0b6ac
arseon21 <ratsvisual@gmail.com>
2026-02-03 12:03:33 +0300

    Pipeline_2

--- app/services/retrieval.py
+++ app/services/retrieval.py
@@ -59,7 +59,6 @@ class VectorStore:
             return []
 
         image = query_image.convert("RGB")
-        # Для CLIPModel в sentence-transformers 5.x изображение передается как элемент списка `sentences`.
         query_emb = self.model.encode([image], convert_to_tensor=True)
         query_emb = _normalize(query_emb)
 
